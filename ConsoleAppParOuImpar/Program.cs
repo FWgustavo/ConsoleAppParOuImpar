5cb0023422faeffcd85e21dcae2afb7f80026089
Usuario <Usuario@CPD02-M01>
2024-03-26 11:01:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppParOuImpar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Impar(1) ou Par(0) ");
+            int escolha = int.Parse(Console.ReadLine());
+            int cont = escolha;
+            Console.Clear();
+
+            while (cont <= 20)
+            {
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
